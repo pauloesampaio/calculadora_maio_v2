4fc28d325302471fc8e9d92e4f63f49e055c5f9f
Paulo Eduardo Sampaio <paulo.ed@gmail.com>
2021-08-16 23:48:59 +0200

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import os
 from catboost import CatBoostRegressor
 from google.oauth2 import service_account
 from google.cloud import storage
